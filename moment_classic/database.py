44d51dd47e3b75f5fd37831aca1e2a2f1f5d78ae
kibeom <inthound@gmail.com>
2025-06-25 11:31:23 +0900

    [hotfix] db url 오류 수정

--- moment_classic/database.py
+++ moment_classic/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-DB_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+DB_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
